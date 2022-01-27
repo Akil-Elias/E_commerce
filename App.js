f5fbd3d5bfbfcc525fa4925121b9e0969b7ac8b5
Kayode <82780978+Akil-Elias@users.noreply.github.com>
2022-01-26 23:31:53 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Products, Navbar } from './Components';
+
+
+
+const App = () => {
+  return (
+      <div>
+          <Navbar />
+          <Products />
+      </div>
+  )
+};
+
+export default App;
